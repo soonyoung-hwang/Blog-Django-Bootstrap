401325dd02b4b9d14685ee20b854cc55a714cbf7
Soonyoung <soonyounghwang92@gmail.com>
2023-10-12 17:52:24 +0900

    feat: Add str of Post

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,6 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField()
     # author = ...
+
+    def __str__(self):
+        return f"[{self.pk}] {self.title}"
