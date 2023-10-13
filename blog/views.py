0f08420a421d44c59c32e9ce5734c48ee744a917
Soonyoung <soonyounghwang92@gmail.com>
2023-10-13 17:19:46 +0900

    feat: Change post detail with PBV to CBV

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -8,13 +7,5 @@ class PostList(ListView):
     ordering = "-pk"
 
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        "blog/single_post_page.html",
-        {
-            "post": post,
-        },
-    )
+class PostDetail(DetailView):
+    model = Post
