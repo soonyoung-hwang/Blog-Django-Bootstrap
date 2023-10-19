13be00e8b66a8f04653491800efc73673cd7e22a
Soonyoung <78343941+soonyoung-hwang@users.noreply.github.com>
2023-10-19 18:24:48 +0900

    내비게이션바, 푸터도 모듈화 하고 기능 구현하기 (#36)
    * bug: Fix minor issue on post detail
    
    * refactor: Modularize navbar and footer test
    
    * feat: Test navbar href
    
    * feat: Implement navbar href
    
    * refactor: Modularize navbar footer htmls

--- blog/tests.py
+++ blog/tests.py
@@ -7,6 +7,25 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def navbar_test(self, soup):
+        # 1.4 내비게이션 바가 있다.
+        navbar = soup.nav
+        # 1.5 Blog, About Me라는 문구가 내비게이션 바에 있다.
+        self.assertIn("Blog", navbar.text)
+        self.assertIn("About Me", navbar.text)
+
+        logo_btn = navbar.find("a", text="Do It Django")
+        self.assertEqual(logo_btn.attrs["href"], "/")
+
+        logo_btn = navbar.find("a", text="Home")
+        self.assertEqual(logo_btn.attrs["href"], "/")
+
+        logo_btn = navbar.find("a", text="Blog")
+        self.assertEqual(logo_btn.attrs["href"], "/blog/")
+
+        logo_btn = navbar.find("a", text="About Me")
+        self.assertEqual(logo_btn.attrs["href"], "/about_me/")
+
     def test_post_list(self):
         # 1.1 포스트 목록 페이지를 가져온다.
         response = self.client.get("/blog/")
@@ -15,11 +34,9 @@ class TestView(TestCase):
         # 1.3 페이지 타이틀은 'Blog'이다.
         soup = BeautifulSoup(response.content, "html.parser")
         self.assertEqual(soup.title.text, "Blog")
-        # 1.4 내비게이션 바가 있다.
-        navbar = soup.nav
-        # 1.5 Blog, About Me라는 문구가 내비게이션 바에 있다.
-        self.assertIn("Blog", navbar.text)
-        self.assertIn("About Me", navbar.text)
+
+        # 1.4 navbar 테스트
+        self.navbar_test(soup)
 
         # 2.1 메인 영역에 게시물이 하나도 없다면
         self.assertEqual(Post.objects.count(), 0)
@@ -59,10 +76,8 @@ class TestView(TestCase):
         response = self.client.get(post_001.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, "html.parser")
-        # 2.2 포스트 목록 페이지와 똑같은 내비게이션 바가 있다.
-        navbar = soup.nav
-        self.assertIn("Blog", navbar.text)
-        self.assertIn("About Me", navbar.text)
+        # 2.2 navbar 테스트
+        self.navbar_test(soup)
         # 2.3 첫 번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어있다.
         self.assertIn(post_001.title, soup.title.text)
         # 2.4 첫 번째 포스트의 제목이 포스트 영역에 있다.
