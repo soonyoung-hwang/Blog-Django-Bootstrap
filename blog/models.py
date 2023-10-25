f2dfb9a0647f03c1377ff255d25350def0250edc
Soonyoung <78343941+soonyoung-hwang@users.noreply.github.com>
2023-10-25 13:46:22 +0900

    Tag 기능 추가하고 Tag 페이지 만들기 (#42)
    * feat: Create tag class
    
    * feat: Update test for tag
    
    * feat: Update postlist for tag
    
    * feat: Update postdetail for tag
    
    * feat: Implement tag page

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,17 @@ from django.contrib.auth.models import User
 import os
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f"/blog/tag/{self.slug}"
+
+
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     # Slug Field는 사람이 읽을 수 있는 텍스트로 고유 URL을 만들고 싶을 때 사용합니다.
@@ -30,6 +41,7 @@ class Post(models.Model):
     category = models.ForeignKey(
         Category, null=True, blank=True, on_delete=models.SET_NULL
     )
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return f"[{self.pk}] {self.title}"
--- blog/tests.py
+++ blog/tests.py
@@ -1,12 +1,13 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+        # create users
         self.user_trump = User.objects.create_user(
             username="Trump", password="somepassword"
         )
@@ -14,18 +15,25 @@ class TestView(TestCase):
             username="Obama", password="somepassword"
         )
 
+        # create categories
         self.category_programming = Category.objects.create(
             name="programming", slug="programming"
         )
         self.category_music = Category.objects.create(name="music", slug="music")
 
-        # 3.1 게시물이 2개 있다면
+        # create tags
+        self.tag_python_kor = Tag.objects.create(name="파이썬 공부", slug="파이썬-공부")
+        self.tag_python = Tag.objects.create(name="Python", slug="python")
+        self.tag_hello = Tag.objects.create(name="Hello", slug="hello")
+
+        # create posts
         self.post_001 = Post.objects.create(
             title="첫 번째 포스트입니다.",
             content="Hello World. We are the one. Thanks",
             author=self.user_trump,
             category=self.category_programming,
         )
+        self.post_001.tags.add(self.tag_hello)
         self.post_002 = Post.objects.create(
             title="두 번째 포스트입니다.",
             content="결과야 빨리 나와라... 시험아 겹치지 마라...",
@@ -37,12 +45,30 @@ class TestView(TestCase):
             content="이건 내용이 카테고리에 넣기 너무 애매하네요.",
             author=self.user_obama,
         )
+        self.post_003.tags.add(self.tag_python)
+        self.post_003.tags.add(self.tag_python_kor)
+
+    def test_tag_page(self):
+        response = self.client.get(self.tag_hello.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        self.navbar_test(soup)
+        self.category_card_test(soup)
+
+        self.assertIn(self.tag_hello.name, soup.h1.text)
+
+        main_area = soup.find("div", id="main-area")
+        self.assertIn(self.tag_hello.name, main_area.text)
+        self.assertIn(self.post_001.title, main_area.text)
+        self.assertNotIn(self.post_002.title, main_area.text)
+        self.assertNotIn(self.post_003.title, main_area.text)
 
     def test_category_page(self):
         response = self.client.get(self.category_programming.get_absolute_url())
         self.assertEqual(response.status_code, 200)
-
         soup = BeautifulSoup(response.content, "html.parser")
+
         self.navbar_test(soup)
         self.category_card_test(soup)
 
@@ -104,17 +130,26 @@ class TestView(TestCase):
         post_001_card = main_area.find("div", id="post-1")
         self.assertIn(self.post_001.title, post_001_card.text)
         self.assertIn(self.post_001.category.name, post_001_card.text)
+        self.assertIn(self.post_001.author.username.upper(), post_001_card.text)
+        self.assertIn(self.tag_hello.name, post_001_card.text)
+        self.assertNotIn(self.tag_python.name, post_001_card.text)
+        self.assertNotIn(self.tag_python_kor.name, post_001_card.text)
 
         post_002_card = main_area.find("div", id="post-2")
         self.assertIn(self.post_002.title, post_002_card.text)
         self.assertIn(self.post_002.category.name, post_002_card.text)
+        self.assertIn(self.post_002.author.username.upper(), post_002_card.text)
+        self.assertNotIn(self.tag_hello.name, post_002_card.text)
+        self.assertNotIn(self.tag_python.name, post_002_card.text)
+        self.assertNotIn(self.tag_python_kor.name, post_002_card.text)
 
         post_003_card = main_area.find("div", id="post-3")
         self.assertIn(self.post_003.title, post_003_card.text)
         self.assertIn("미분류", post_003_card.text)
-
-        self.assertIn(self.user_trump.username.upper(), main_area.text)
-        self.assertIn(self.user_obama.username.upper(), main_area.text)
+        self.assertIn(self.post_003.author.username.upper(), post_003_card.text)
+        self.assertNotIn(self.tag_hello.name, post_003_card.text)
+        self.assertIn(self.tag_python.name, post_003_card.text)
+        self.assertIn(self.tag_python_kor.name, post_003_card.text)
 
         # 포스트가 없는 경우
         Post.objects.all().delete()
@@ -151,10 +186,12 @@ class TestView(TestCase):
         post_area = main_area.find("div", id="post-area")
         self.assertIn(self.post_001.title, post_area.text)
         self.assertIn(self.category_programming.name, post_area.text)
-        # 2.5 첫 번째 포스트의 작성자(author)가 포스트 영역에 있다. (아직 구현할 수 없음)
-        # 아직 작성 불가
         # 2.6 첫 번째 포스트의 내용(content)이 포스트 영역에 있다.
         self.assertIn(self.post_001.content, post_area.text)
 
         # 2.7 User name check
         self.assertIn(self.user_trump.username.upper(), post_area.text)
+
+        self.assertIn(self.tag_hello.name, post_area.text)
+        self.assertNotIn(self.tag_python.name, post_area.text)
+        self.assertNotIn(self.tag_python_kor.name, post_area.text)
