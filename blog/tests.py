1a11cc09c2cd84ce5ca5aebb3656895dc77f3c33
Soonyoung <78343941+soonyoung-hwang@users.noreply.github.com>
2023-10-26 20:15:46 +0900

    사용자가 Tag를 만들어 입력할 수 있게 구현 (#48)
    * feat: Update test for tag creation
    
    * feat: Implement tag creation
    
    * feat: Implement edit tags

--- blog/tests.py
+++ blog/tests.py
@@ -25,8 +25,8 @@ class TestView(TestCase):
 
         # create tags
         self.tag_python_kor = Tag.objects.create(name="파이썬 공부", slug="파이썬-공부")
-        self.tag_python = Tag.objects.create(name="Python", slug="python")
-        self.tag_hello = Tag.objects.create(name="Hello", slug="hello")
+        self.tag_python = Tag.objects.create(name="python", slug="python")
+        self.tag_hello = Tag.objects.create(name="hello", slug="hello")
 
         # create posts
         self.post_001 = Post.objects.create(
@@ -70,14 +70,26 @@ class TestView(TestCase):
         main_area = soup.find("div", id="main-area")
         self.assertIn("Create New Post", main_area.text)
 
+        tag_str_input = main_area.find("input", id="id_tags_str")
+        self.assertTrue(tag_str_input)
+
         self.client.post(
             "/blog/create_post/",
-            {"title": "Post Form 만들기", "content": "Post Form 페이지를 만듭시다."},
+            {
+                "title": "Post Form 만들기",
+                "content": "Post Form 페이지를 만듭시다.",
+                "tags_str": "new tag; 한글 태그, python",
+            },
         )
         self.assertEqual(Post.objects.count(), 4)
         last_post = Post.objects.last()
         self.assertEqual(last_post.title, "Post Form 만들기")
         self.assertEqual(last_post.author.username, "Obama")
+
+        self.assertEqual(last_post.tags.count(), 3)
+        self.assertTrue(Tag.objects.get(name="new tag"))
+        self.assertTrue(Tag.objects.get(name="한글 태그"))
+        self.assertEqual(Tag.objects.count(), 5)
 
     def test_update_post(self):
         update_post_url = f"/blog/update_post/{self.post_003.pk}/"
@@ -104,12 +116,17 @@ class TestView(TestCase):
         main_area = soup.find("div", id="main-area")
         self.assertIn("Edit Post", main_area.text)
 
+        tag_str_input = main_area.find("input", id="id_tags_str")
+        self.assertTrue(tag_str_input)
+        self.assertIn("파이썬 공부; python", tag_str_input.attrs["value"])
+
         response = self.client.post(
             update_post_url,
             {
                 "title": "세 번째 포스트 수정했습니다.",
                 "content": "안녕 세계? 우리는 하나!",
                 "category": self.category_music.pk,
+                "tags_str": "파이썬 공부; 한글 태그, some tag",
             },
             follow=True,
         )
@@ -118,6 +135,10 @@ class TestView(TestCase):
         self.assertIn("세 번째 포스트 수정했습니다.", main_area.text)
         self.assertIn("안녕 세계? 우리는 하나!", main_area.text)
         self.assertIn(self.category_music.name, main_area.text)
+        self.assertIn("파이썬 공부", main_area.text)
+        self.assertIn("한글 태그", main_area.text)
+        self.assertIn("some tag", main_area.text)
+        self.assertNotIn("python", main_area.text)
 
     def test_tag_page(self):
         response = self.client.get(self.tag_hello.get_absolute_url())
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post, Category, Tag
 from django.core.exceptions import PermissionDenied
+from django.utils.text import slugify
 
 
 class PostList(ListView):
@@ -46,7 +47,23 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
             current_user.is_superuser or current_user.is_staff
         ):
             form.instance.author = current_user
-            return super(PostCreate, self).form_valid(form)
+            response = super(PostCreate, self).form_valid(form)
+            tags_str = self.request.POST.get("tags_str")
+
+            if tags_str:
+                tags_str = tags_str.strip()
+                tags_str = tags_str.replace(",", ";")
+                tags_list = tags_str.split(";")
+
+                for t in tags_list:
+                    t = t.strip()
+                    tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                    if is_tag_created:
+                        tag.slug = slugify(t, allow_unicode=True)
+                        tag.save()
+                    self.object.tags.add(tag)
+
+            return response
         else:
             return redirect("/blog/")
 
@@ -60,16 +77,45 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
         "head_image",
         "file_upload",
         "category",
-        "tags",
     ]
 
     template_name = "blog/post_update_form.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(PostUpdate, self).get_context_data()
+        if self.object.tags.exists():
+            tags_str_list = list()
+            for t in self.object.tags.all():
+                tags_str_list.append(t.name)
+            context["tags_str_default"] = "; ".join(tags_str_list)
+
+        return context
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated and request.user == self.get_object().author:
             return super(PostUpdate, self).dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
+
+    def form_valid(self, form):
+        response = super(PostUpdate, self).form_valid(form)
+        self.object.tags.clear()
+
+        tags_str = self.request.POST.get("tags_str")
+        if tags_str:
+            tags_str = tags_str.strip()
+            tags_str = tags_str.replace(",", ";")
+            tags_list = tags_str.split(";")
+
+            for t in tags_list:
+                t = t.strip()
+                tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                if is_tag_created:
+                    tag.slug = slugify(t, allow_unicode=True)
+                    tag.save()
+                self.object.tags.add(tag)
+
+        return response
 
 
 def category_page(request, slug):
