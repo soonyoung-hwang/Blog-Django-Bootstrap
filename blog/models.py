359c2666f2b7595eda0a3ce9d13e21fa8ce5de43
Soonyoung <soonyounghwang92@gmail.com>
2023-10-27 18:51:27 +0900

    feat: Apply markdown to post detail

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from markdownx.models import MarkdownxField
+from markdownx.utils import markdown
 import os
 
 
@@ -55,3 +56,6 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split(".")[-1]
+
+    def get_content_markdown(self):
+        return markdown(self.content)
