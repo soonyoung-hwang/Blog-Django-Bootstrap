b7c052c3ac1c490639dc956bbff8a2ce047722ad
Soonyoung <soonyounghwang92@gmail.com>
2023-10-27 17:19:08 +0900

    chore: Include crispy

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_extensions",
+    "crispy_forms",
     "blog",
     "single_pages",
 ]
@@ -123,6 +124,7 @@ STATIC_URL = "static/"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "_media")
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 
 # Default primary key field type
