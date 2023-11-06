0ce6da2e426c7ad6689ff6588d52fde63ebd3658
Soonyoung <soonyounghwang92@gmail.com>
2023-11-06 21:21:07 +0900

    feat: Add avatar to comment

--- blog/models.py
+++ blog/models.py
@@ -73,3 +73,9 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f"{self.post.get_absolute_url()}#comment-{self.pk}"
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f"https://doitdjango.com/avatar/id/1760/ee208b31f61c0f58/svg/{self.author.email}"
